d5a798d166543395175fee8114ae85388d7908de
Diego Reyes <diegorele13@gmail.com>
2023-05-04 17:35:57 -0500

    feat: aboutme

---
+++ components/AboutMe.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="about-me">
+    <h1>Miniblog</h1>
+    <p>
+      Miniblog de la muerte es un pequeño sistema de blog personal hecho con
+      Nuxt, Netlify y Airtable
+    </p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.about-me {
+  @apply w-full flex flex-col justify-center items-center px-4 py-16 bg-gray-200;
+  h1 {
+    @apply font-black text-4xl text-gray-900 pb-2;
+  }
+  p {
+    @apply text-lg py-2;
+  }
+}
+</style>
