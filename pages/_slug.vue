4130aedc3eaaf9d8df21b2de8b61893e953af577
Diego Reyes <diegorele13@gmail.com>
2023-05-09 11:43:49 -0500

    done: individual blog

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -19,23 +19,45 @@
 <script>
 export default {
   name: 'ArticlePage',
+  asyncData({ params, $http }) {
+    const { slug } = params
+    const article = $http.$get(
+      `http://localhost:9999/.netlify/functions/article?slug=${slug}`
+    )
+    // eslint-disable-next-line no-console
+    console.log(article)
+
+    return article
+  },
   data: () => ({
-    post: {
-      slug: 'mi-primer-post',
-      title: 'Mi primer post',
-      author: 'Diego Reyes',
-      updated: '05/05/2023',
-      description: 'Lorem ipsum dolor sit amet, consectetur',
-      cover: 'https://via.placeholder.com/1024x420',
-      content:
-        '#title\n\n##Second title\n\nLorem ipsum dolor sit amet, consectetur',
-    },
+    // post: {
+    //   slug: 'mi-primer-post',
+    //   title: 'Mi primer post',
+    //   author: 'Diego Reyes',
+    //   updated: '05/05/2023',
+    //   description: 'Lorem ipsum dolor sit amet, consectetur',
+    //   cover: 'https://via.placeholder.com/1024x420',
+    //   content:
+    //     '#title\n\n##Second title\n\nLorem ipsum dolor sit amet, consectetur',
+    // },
   }),
   head() {
     return {
       title: this.post?.title,
       meta: [{ name: 'description', content: this.post?.description || '' }],
     }
+  },
+  computed: {
+    post() {
+      return {
+        title: this.article?.title,
+        author: this.article['author-name'][0],
+        updated: new Date(this.article?.updated).toLocaleDateString(),
+        description: this.article?.description,
+        cover: this.article.cover[0].thumbnails.full.url,
+        content: this.article?.content,
+      }
+    },
   },
 }
 </script>
